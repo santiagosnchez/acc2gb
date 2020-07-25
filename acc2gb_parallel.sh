94f0e028d11258c9e3881a368b0eea9228b01830
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-07-24 23:51:02 -0400

    Improvements and streamlining

--- acc2gb_parallel.sh
+++ acc2gb_parallel.sh
@@ -13,26 +13,46 @@ First run without arguments (API key)
 
 Then try:
 chmod +x acc2gb_parallel.sh
-./acc2gb_parallel.sh accession_list.txt new_genbank_file.gb
+./acc2gb_parallel.sh -list accession_list.txt -gb new_genbank_file.gb
 "
 
-if [[ "$*" == -h* ]]; then
-	die "$help_message"
-fi
+download_files()
+{
+    args = $@
+    acclist = ${args[0]}
+    gbfile = ${args[1]}
+    echo "Downloading to ${gblist}"
+    cat ${acclist} | parallel --progress "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'${NCBI_API_KEY}&rettype=gb&retmode=text'" > $2
+    echo -e "\nNumber of records downloaded:  $(grep -c '//' ${gblist})"
+    echo "Number of records not found: $(grep -c 'Failed to retrieve sequence' ${gbfile})"
+}
+
+# first ask for the API key
+# and store it in ~/.bashrc
 
 source ~/.bashrc
 if [[ -z $NCBI_API_KEY ]]; then
-        echo -n "An api key is required. If you don't have one, go to https://www.ncbi.nlm.nih.gov/myncbi/ and request one.
+    echo -n "An api key is required. If you don't have one, go to https://www.ncbi.nlm.nih.gov/myncbi/ and request one.
 If you do have one, paste the key here: "
-        read api_key
-        echo "export NCBI_API_KEY=$api_key" >> ~/.bashrc
-        die "Key stored in ~/.bashrc"
+    read api_key
+    echo "export NCBI_API_KEY=$api_key" >> ~/.bashrc
+    die "Key stored in ~/.bashrc"
 else
-        echo "Key found: $NCBI_API_KEY"
+    echo "Key found: $NCBI_API_KEY"
 fi
 
-if [[ "$*" == '' ]]; then
-        die "$help_message"
+# continue if key is found
+# go through arguments
+
+acclist=''
+gbfile=''
+if [[ "$*" == -h* || "$*" == '' ]]; then
+    die "$help_message"
+else
+    for i in `seq 0 $#`; do
+        if [[ "${@[$i]}" == "-list" ]]; then
+            $acclist=${@[$(( i+1 ))]}
+    fi
 fi
 
 # check if gb file has not been created already
@@ -60,11 +80,7 @@ if [[ $check == 0 ]]; then
 	if [[ ${#dupl} == 0 ]]; then
 
 		# retreive data
-
-		echo "Downloading to $2..."
-		cat $1 | parallel --progress "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'${NCBI_API_KEY}&rettype=gb&retmode=text'" > $2
-		echo -e "\nNumber of records downloaded:  $(grep -c '//' $2)"
-		echo "Number of records not found: $(grep -c 'Failed to retrieve sequence' $2)"
+		download_files 
 	else
 		# show duplicates
 
