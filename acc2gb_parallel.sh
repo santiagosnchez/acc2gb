6c0717c04ea9e6a1a8417de31ebdc62b9e73b4f0
santiagosnchez <santiago.snchez@gmail.com>
2020-07-26 01:53:44 -0400

    updated help, messages, and threading

--- acc2gb_parallel.sh
+++ acc2gb_parallel.sh
@@ -13,16 +13,51 @@ First run without arguments (API key)
 
 Then try:
 chmod +x acc2gb_parallel.sh
-./acc2gb_parallel.sh -list accession_list.txt -gb new_genbank_file.gb
+./acc2gb_parallel.sh [ -h | -threads INT -gb OUTPUTFILE ] -list INPUTFILE
+
+[-h|-help]      Prints this help message
+-list    STR    Path to the file with a list of GenBank accessions
+(optional)
+-gb      STR    Path/name to/of the GenBank output file
+                If empty, the default name is \"out_genbank.gb\"
+-threads INT    Number of threads to use. Default is to use all threads.
 "
+
+check_parallel()
+{
+    parallel -V &> /dev/null
+    if [[ $? != 0 ]]; then
+        echo -en "gnu-parallel is not installed. See https://www.gnu.org/software/parallel/ for installations.
+Run single threaded?[y|n]:"
+        read single
+        if [[ "$single" == "n" ]]; then
+            die "Exiting..."
+        else
+            echo 1
+        fi
+    else
+        echo 0
+    fi
+}
 
 download_files()
 {
-    args = $@
-    acclist = ${args[0]}
-    gbfile = ${args[1]}
+    args=($@)
+    acclist=${args[0]}
+    gbfile=${args[1]}
+    threads=${args[2]}
     echo "Downloading to ${gblist}"
-    cat ${acclist} | parallel --progress "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'${NCBI_API_KEY}&rettype=gb&retmode=text'" > $2
+    if [[ $threads == 0 ]]; then
+        cat ${acclist} | parallel --progress \ 
+        "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'&api_key=${NCBI_API_KEY}&rettype=gb&retmode=text'" > $gbfile
+    elif [[ $threads == 1 ]]l then
+        for acc in `cat ${acclist}`; do
+            curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${acc}&api_key=${NCBI_API_KEY}&rettype=gb&retmode=text"
+        done > $gbfile
+    else
+       cat ${acclist} | parallel --progress -j $threads \ 
+       "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'&api_key=${NCBI_API_KEY}&rettype=gb&retmode=text'" > $gbfile
+    fi
     echo -e "\nNumber of records downloaded:  $(grep -c '//' ${gblist})"
     echo "Number of records not found: $(grep -c 'Failed to retrieve sequence' ${gbfile})"
 }
@@ -41,63 +76,75 @@ else
     echo "Key found: $NCBI_API_KEY"
 fi
 
+# chech if gnu-parallel is installed
+threads=`check_parallel`
+
 # continue if key is found
 # go through arguments
 
 acclist=''
 gbfile=''
+threads=''
+args=($@)
 if [[ "$*" == -h* || "$*" == '' ]]; then
     die "$help_message"
 else
     for i in `seq 0 $#`; do
-        if [[ "${@[$i]}" == "-list" ]]; then
-            $acclist=${@[$(( i+1 ))]}
-    fi
+        if [[ "${args[$i]}" == '-list' ]]; then
+            acclist=${args[$(( i+1 ))]}
+        elif [[ "${args[$i]}" == '-gb' ]]; then
+            gbfile=${args[$(( i+1 ))]}
+        elif [[ "${args[$i]}" == '-threads' ]]; then
+            threads=${args[$(( i+1 ))]}
+        else
+            die "$help_message"
+        fi
+    done
+fi
+# check arg variables
+if [[ ${#acclist} == 0 ]]; then
+    die "No file name given to accession list"
+fi
+if [[ ${#acclist} == 0 ]]; then
+    gbfile="out_genbank.gb"
+fi
+if [[ ${#threads} == 0 ]]; then
+    threads=0
 fi
 
 # check if gb file has not been created already
 
-if [ -f $2 ]; then
-	echo -n "File $2 already exists, replace?[y|n]:"
-	read replace
-	if [[ $replace == "y" ]]; then
-		rm $2
-	else 
-		die "Exiting...\n"
-	fi
+if [ -f $gbfile ]; then
+    echo -n "File $gbfile already exists, replace?[y|n]:"
+    read replace
+    if [[ $replace == "y" ]]; then
+        rm $gbfile
+    else 
+        die "Exiting...\n"
+    fi
 fi
 
 # check if list is empty
 
-check=`[ -s $1 ]; echo $?`
+check=`[ -s $acclist ]; echo $?`
 
 if [[ $check == 0 ]]; then
-
-	# check for duplicates
-
-	dupl=`sort $1 | uniq -c | awk '$1 !~ 1'`
-
-	if [[ ${#dupl} == 0 ]]; then
-
-		# retreive data
-		download_files 
-	else
-		# show duplicates
-
-		echo -e "\nDuplicates:\n$dupl"
-
-		# create new list
-
-		sort $1 | uniq -c | awk '{print $2}' > "unique_$1"
-		echo -e "\nA new list file uniq_$1 was created\n\n"
-		
-		# retreive data
-
-		echo "Downloading to $2..."
-		cat "unique_$1" | parallel --progress "curl -s 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id='{}'&${NCBI_API_KEY}&rettype=gb&retmode=text'" > $2
-		echo -e "\nNumber of records downloaded:  $(grep -c '//' $2)"
-		echo "Number of records not found: $(grep -c 'Failed to retrieve sequence' $2)"
-	fi
+    # check for duplicates
+    echo "Checking for duplicates..."
+    dupl=`sort $acclist | uniq -c | awk '$1 !~ 1'`
+    if [[ ${#dupl} == 0 ]]; then
+        # retreive data
+        echo "Downloading to $gbfile..."
+        download_files $acclist $gbfile $threads
+    else
+        # show duplicates
+        echo -e "\nDuplicates:\n$dupl"
+        # create new list
+        sort $acclist | uniq > "unique_${acclist}"
+        echo -e "\nA new list file uniq_${acclist} was created\n\n"	
+        # retreive data
+        echo "Downloading to $gbfile..."
+        download_files $acclist $gbfile $threads
 else
-	die "File: $1 is empty. Exiting...\n"
+	die "File: $acclist is empty. Exiting...\n"
 fi
